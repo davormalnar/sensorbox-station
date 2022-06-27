321e8dddfef309223c259fb0cc80f75623ed39a2
Davor Malnar <davor.malnar@gmail.com>
2022-06-27 20:06:48 +0200

    fixes for AQI warn script; set +x flag for scripts

--- sensors/lib/warnAqi.py
+++ sensors/lib/warnAqi.py
@@ -1,28 +1,48 @@
 #!/usr/bin/python3
 import os
 
-def color(aqiVal):
-  if aqiVal < 51:
+def severity25(pmVal):
+  if pmVal == 0:
+    return "Error"
+  elif pmVal < 13:
     return "Good"
-  elif aqiVal < 101:
+  elif pmVal < 36:
     return "Moderate"
-  elif aqiVal < 151:
+  elif pmVal < 56:
     return "Unhealthy (!)"
-  elif aqiVal < 201:
+  elif pmVal < 150:
     return "Unhealthy (!!)"
-  elif aqiVal < 301:
+  elif pmVal < 250:
+    return "Very unhealthy (!!!)"
+  elif pmVal <= 500:
     return "Hazardous"
-  elif aqiVal > 401:
+  elif pmVal > 500:
     return "Extremely hazardous"
 
-  return ""
+def severity10(pmVal):
+  if pmVal == 0:
+    return "Error"
+  elif pmVal < 55:
+    return "Good"
+  elif pmVal < 155:
+    return "Moderate"
+  elif pmVal < 255:
+    return "Unhealthy (!)"
+  elif pmVal < 355:
+    return "Unhealthy (!!)"
+  elif pmVal < 425:
+    return "Very unhealthy (!!!)"
+  elif pmVal <= 604:
+    return "Hazardous"
+  elif pmVal > 604:
+    return "Extremely hazardous"
 
 def notify(aqi):
-  msg = "============== WARNING ============== \n" + \
-        "-> station: " + str(aqi['stationName']) + " \n" + \
-        str(color(aqi['aqiPM25'])) + " - AQI (PM 2.5): *" + str(aqi['aqiPM25']) + "* (" + str(aqi['pm25']) + " ug/m3) \n" + \
-        str(color(aqi['aqiPM10'])) + " - AQI (PM 10): *" + str(aqi['aqiPM10']) + "* (" + str(aqi['pm10']) + " ug/m3) \n" + \
-        "========== " + str(aqi['created']) + " ========== \n"
+  msg = "==================== WARNING ==================== \n" + \
+        "-> " + str(aqi['stationName']) + " \n" + \
+        str(severity25(aqi['pm25'])) + " - AQI (PM 2.5): *" + str(aqi['aqiPM25']) + "* (" + str(aqi['pm25']) + " ug/m3) \n" + \
+        str(severity10(aqi['pm10'])) + " - AQI (PM 10): *" + str(aqi['aqiPM10']) + "* (" + str(aqi['pm10']) + " ug/m3) \n" + \
+        "================ " + str(aqi['created']) + " ================ \n"
 
   os.system('/home/pi/sensorbox-station/scripts/telegramBotNotify "' + msg + '"')
 
